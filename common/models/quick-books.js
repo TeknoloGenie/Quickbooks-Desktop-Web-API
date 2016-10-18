ba217fa1f6dfe2255061388174261a990b380c66
Aaron Russell <aaron@nodegeeksllc.com>
2016-10-18 04:34:29 -0700

    demo day commit

--- common/models/quick-books.js
+++ common/models/quick-books.js
@@ -4,6 +4,7 @@ module.exports = function (QuickBooks) {
 
   QuickBooks.queueTransaction = function (body, cb) {
     QuickBooks.getDataSource('quickbooksService').connector.webService.queueTransaction({modelName: body.modelName, method: body.method, data: body.data, filter: body.filter});
+    cb();
   };
 
   QuickBooks.remoteMethod(
@@ -14,6 +15,19 @@ module.exports = function (QuickBooks) {
       ],
       description: 'Queue the request transaction for syncing with QuickBooks',
       http: {path: '/queueTransaction', verb: 'post'}
+    }
+  );
+
+  QuickBooks.reSync = function (cb) {
+    QuickBooks.getDataSource('quickbooksService').connector.webService.reSync();
+    cb();
+  };
+
+  QuickBooks.remoteMethod(
+    'reSync',
+    {
+      description: 'Re-Sync all the QuickBooks data into the application',
+      http: {path: '/reSync', verb: 'get'}
     }
   );
 };
